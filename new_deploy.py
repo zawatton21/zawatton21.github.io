49192ef02d8597a4cfd1d7cf57051812cf5514ac
zawatton21 <kurozawawo@gmail.com>
2022-04-22 15:20:00 +0900

    blog updated on 20220422

---
+++ new_deploy.py
@@ -0,0 +1,13 @@
+import os
+import datetime
+import subprocess as cmd
+
+#input values
+new_date = datetime.datetime.now().strftime('%Y%m%d')
+current_dir = os.getcwd()
+
+#Subprocessg��
+cp = cmd.run(f"git -C {current_dir} add .", check=True, shell=True)
+cp = cmd.run(f"git -C {current_dir} commit -m 'blog updated on {new_date}'", check=True, shell=True)
+cp = cmd.run(f"git -C {current_dir} push origin master", check=True, shell=True)
+print('Githubxn����!')
